01c152eeda9d5b052106153650d3fbdc0779ffac
saiyam-gambhir <saiyam@vinsol.com>
2018-06-20 13:59:52 +0530

    Header component and styles

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <main id="app">
+    <app-header></app-header>
   </main>
 </template>
 
 <script>
+import Header from './components/shared/Header.vue';
 
 export default {
   name: 'app',
+  components: {
+    appHeader: Header
+  }
 }
 </script>
 
 <style>
+  @import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+  @import './assets/css/styles.css';
 </style>
---
+++ src/components/shared/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="primary-header">
+    <div class="container relative">
+
+        <div class="primary-header__basket absolute active">
+          <a href="javascript:void(0);" class="absolute primary-header__basket-link">
+            <img src="../../assets/images/basket.svg" alt="" class="absolute">
+          </a>
+        </div>
+
+        <div class="brand absolute">
+          <a href="#">
+          </a>
+        </div>
+      
+        <nav class="primary-header__nav">
+          <ul class="primary-header__links list-inline text-uppercase text-center margin-0">
+            <li class="primary-header__link with-sub-menu">
+              <a href="javascript:void(0);" class="link">Link</a>
+              <ul class="absolute primary-header__sub-menu padding-0 text-uppercase">
+                <li><a href="javascript:void(0);" class="no-underline">Link</a></li>
+                <li><a href="javascript:void(0);" class="no-underline">Link</a></li>
+                <li><a href="javascript:void(0);" class="no-underline">Link</a></li>
+              </ul>
+            </li>
+            <li class="primary-header__link">
+              <a href="javascript:void(0);" class="link">Link</a>
+            </li>
+            <li class="primary-header__link">
+              <a href="javascript:void(0);" class="link">Link</a>
+            </li>
+            <li class="primary-header__link">
+              <a href="javascript:void(0);" class="link">Link</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'app-header'
+  }
+</script>
